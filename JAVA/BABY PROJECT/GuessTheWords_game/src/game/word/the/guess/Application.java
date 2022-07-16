9c83d0c3bb7eed3c318255dd45607a7e0b1716ff
BiswasAmitav <biswasamit2648@gmail.com>
2022-07-16 15:26:31 +0530

    all function is perfectly working

--- JAVA/BABY PROJECT/GuessTheWords_game/src/game/word/the/guess/Application.java
+++ JAVA/BABY PROJECT/GuessTheWords_game/src/game/word/the/guess/Application.java
@@ -2,7 +2,12 @@ package game.word.the.guess;
 
 public class Application {
     public static void main(String[] args) {
-        GuessTheWord game=new GuessTheWord();
+//        GuessTheWord game=new GuessTheWord();
+//        game.start();
+
+
+        GuessTheWord game = new GuessTheWord();
         game.start();
+        game.end();
     }
 }
--- JAVA/BABY PROJECT/GuessTheWords_game/src/game/word/the/guess/Words.java
+++ JAVA/BABY PROJECT/GuessTheWords_game/src/game/word/the/guess/Words.java
@@ -1,14 +1,107 @@
 package game.word.the.guess;
 
+import java.util.Random;
+
 public class Words {
-    String[] randomWords={"animals","happiness","love", "politices","properties","ceremony","independence","benetha",
+    /*
+    =================================
+    EXPERIMENTAL CODE...
+    ===========================================
+
+
+    String[] randomWords={"animals","happiness","love", "politics","properties","ceremony","independence","benetha",
         "education","Mba"};
     private String selectedWord;
 
+    private Random random = new Random();
+
+    private char[] letters;
+
     public Words(){
-        selectedWord = randomWords[3];
+        selectedWord = randomWords[random.nextInt(randomWords.length)];
+        letters = new char[selectedWord.length()];
     }
     public String toString(){
-        return randomWords[1];
+//        String word = "";
+//        letters[2]='a';
+
+
+        //INSTEAD OF THIS WE COULD MAKE STRING BUILDER....
+
+        StringBuilder text = new StringBuilder();
+
+        for(char letter:letters){
+            if (letter=='\u0000'){
+                //word +='-';
+                text.append('-');
+            }
+            else{
+                //word +=letter;
+                text.append(letter);
+            }
+            text.append(' ');
+        }
+        return text.toString();
+    }
+
+    */
+
+
+
+
+
+
+
+    //=============================
+//    FIANAL CODE..............
+//    ================================
+
+    private String[] randomWords = { "animals", "happiness", "indefinite", "steady", "birthday", "extreme", "rights",
+            "properties", "ceremony", "independence", "beneath", "information", "students", "employee" };
+
+    private String selectedWord;
+    private Random random = new Random();
+    private char[] letters;
+
+    public Words() {
+        selectedWord = randomWords[random.nextInt(randomWords.length)];
+        letters = new char[selectedWord.length()];
+    }
+
+    public String toString() {
+
+        StringBuilder text = new StringBuilder();
+
+        for (char letter : letters) {
+
+            text.append(letter == '\u0000' ? '-' : letter);
+            text.append(' ');
+        }
+
+        return text.toString();
+    }
+
+    public boolean isGuessedRight() {
+
+        for (char letter : letters) {
+            if (letter == '\u0000') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean guess(char letter) {
+
+        boolean guessedRight = false;
+
+        for (int i = 0; i < selectedWord.length(); i++) {
+            if (letter == selectedWord.charAt(i)) {
+                letters[i] = letter;
+                guessedRight = true;
+            }
+        }
+        return guessedRight;
+
     }
 }
